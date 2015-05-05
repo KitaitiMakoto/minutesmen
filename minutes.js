1952ba16d6875be96b276d249dd9b7f03ac28936
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-05-05 16:48:32 +0900

    Move all variables in DOMContentLoaded scope

--- minutes.js
+++ minutes.js
@@ -7,10 +7,10 @@ if (! SpeechRecognition) {
           "You can post message only by text input for now.");
 }
 
-var logger, iconField, nameField, langField, messageField;
 
 document.addEventListener("DOMContentLoaded", function DOMContentLoaded() {
     var ws = io();
+    var logger, iconField, nameField, langField, messageField;
     var speechStreams = createSpeechStream(ws).tee();
     logger = document.getElementById("log");
     var logWriter = attachLogger(logger);
@@ -63,115 +63,115 @@ document.addEventListener("DOMContentLoaded", function DOMContentLoaded() {
             recognition.stop();
         };
     }
-});
 
-function getLogs() {
-    return new Promise(function getLogs(resolve, reject) {
-        var request = new XMLHttpRequest();
-        request.open("GET", "/logs");
-        request.setRequestHeader("accept", "application/json");
-        request.onload = function onload() {
-            if (this.status === 200) {
-                resolve(JSON.parse(this.responseText));
-            } else {
-                reject(new Error(request.statusText));
-            }
-        };
-        request.onerror = function onerror() {
-            reject(new Error(request.statusText));
-        };
-        request.send();
-    });
-}
-
-function createSpeechStream(ws) {
-    return new ReadableStream({
-        start: function startSpeechStream(controller) {
-            ws.on("speech", function onSpeech(message) {
-                controller.enqueue(message);
-            });
-        },
-        error: function errorSpeechStream() {
-            controller.error(new Error("Speech stream error"));
-        }
-    });
-}
-
-function createFormSubmitStream(form) {
-    return new ReadableStream({
-        start: function submitSpeech(controller) {
-            form.addEventListener("submit", function submit(event) {
-                var data = {
-                    name: nameField.value,
-                    speech: messageField.value
-                };
-                controller.enqueue(data);
-                event.preventDefault();
-            });
-        }
-    });
-}
-
-function createSpeechRecognitionStream(recognition) {
-    return new ReadableStream({
-        start: function startSpeechRecognition(controller) {
-            recognition.onresult = function postSpeech(event) {
-                var results = event.results;
-                var i = event.resultIndex, l = results.length, result;
-                for (; i < l; i++) {
-                    result = results[i].item(0);
-                    if (result.confidence > 0.3) {
-                        var data = {
-                            name: nameField.value,
-                            speech: result.transcript
-                        }
-                        controller.enqueue(data);
-                    }
+    function getLogs() {
+        return new Promise(function getLogs(resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.open("GET", "/logs");
+            request.setRequestHeader("accept", "application/json");
+            request.onload = function onload() {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject(new Error(request.statusText));
                 }
             };
-        },
-        cancel: function cancelSpeechRecognition(reason) {
-            recognition.stop();
-        }
-    });
-}
+            request.onerror = function onerror() {
+                reject(new Error(request.statusText));
+            };
+            request.send();
+        });
+    }
 
-function attachLogger(loggerElem) {
-    return new WritableStream({
-        write: function log(message) {
-            var li = document.createElement("li");
-            li.textContent = "[" +
-                (new Date(Number(message.time))).toLocaleString() +
-                "]" + message.name +
-                ": " + message.speech;
-            loggerElem.appendChild(li);
-        }
-    });
-}
+    function createSpeechStream(ws) {
+        return new ReadableStream({
+            start: function startSpeechStream(controller) {
+                ws.on("speech", function onSpeech(message) {
+                    controller.enqueue(message);
+                });
+            },
+            error: function errorSpeechStream() {
+                controller.error(new Error("Speech stream error"));
+            }
+        });
+    }
 
-function createConsoleSpeechLogger() {
-    return new WritableStream({
-        write: function logSpeech(message) {
-            console.log("speech", message);
-        }
-    });
-}
+    function createFormSubmitStream(form) {
+        return new ReadableStream({
+            start: function submitSpeech(controller) {
+                form.addEventListener("submit", function submit(event) {
+                    var data = {
+                        name: nameField.value,
+                        speech: messageField.value
+                    };
+                    controller.enqueue(data);
+                    event.preventDefault();
+                });
+            }
+        });
+    }
 
-// TODO: Extract ack part
-// TODO: Extract message field handle
-function createSpeechPoster(ws) {
-    return new WritableStream({
-        write: function postSpeech(data) {
-            ws.emit("speech", data, function onack(message) {
-                console.log("ack", message);
-                var log = document.createElement("li");
-                log.textContent = "[" +
+    function createSpeechRecognitionStream(recognition) {
+        return new ReadableStream({
+            start: function startSpeechRecognition(controller) {
+                recognition.onresult = function postSpeech(event) {
+                    var results = event.results;
+                    var i = event.resultIndex, l = results.length, result;
+                    for (; i < l; i++) {
+                        result = results[i].item(0);
+                        if (result.confidence > 0.3) {
+                            var data = {
+                                name: nameField.value,
+                                speech: result.transcript
+                            }
+                            controller.enqueue(data);
+                        }
+                    }
+                };
+            },
+            cancel: function cancelSpeechRecognition(reason) {
+                recognition.stop();
+            }
+        });
+    }
+
+    function attachLogger(loggerElem) {
+        return new WritableStream({
+            write: function log(message) {
+                var li = document.createElement("li");
+                li.textContent = "[" +
                     (new Date(Number(message.time))).toLocaleString() +
                     "]" + message.name +
                     ": " + message.speech;
-                logger.appendChild(log);
-            });
-            messageField.value = "";
-        }
-    });
-}
+                loggerElem.appendChild(li);
+            }
+        });
+    }
+
+    function createConsoleSpeechLogger() {
+        return new WritableStream({
+            write: function logSpeech(message) {
+                console.log("speech", message);
+            }
+        });
+    }
+
+    // TODO: Extract ack part
+    // TODO: Extract message field handle
+    function createSpeechPoster(ws) {
+        return new WritableStream({
+            write: function postSpeech(data) {
+                ws.emit("speech", data, function onack(message) {
+                    console.log("ack", message);
+                    var log = document.createElement("li");
+                    log.textContent = "[" +
+                        (new Date(Number(message.time))).toLocaleString() +
+                        "]" + message.name +
+                        ": " + message.speech;
+                    logger.appendChild(log);
+                });
+                messageField.value = "";
+            }
+        });
+    }
+});
